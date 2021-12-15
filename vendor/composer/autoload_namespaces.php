5f176951e8bbbe8ee3b0c9032bd4babc68e8bed4
Shift <shift@laravelshift.com>
2021-12-15 19:49:46 +0000

    Bump Laravel version constraint

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -8,7 +8,6 @@ $baseDir = dirname($vendorDir);
 return array(
     'gburtini\\Distributions' => array($vendorDir . '/gburtini/distributions/src'),
     'Snowplow\\RefererParser' => array($vendorDir . '/snowplow/referer-parser/php/src'),
-    'Mockery' => array($vendorDir . '/mockery/mockery/library'),
     'Evenement' => array($vendorDir . '/evenement/evenement/src'),
     'Detection' => array($vendorDir . '/mobiledetect/mobiledetectlib/namespaced'),
     'Clue\\Redis\\Protocol' => array($vendorDir . '/clue/redis-protocol/src'),
